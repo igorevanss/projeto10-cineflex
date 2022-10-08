6b4e8895558278952bd4d0977cde3f1af141b5c0
igorevanss <igorevans.pro@gmail.com>
2022-10-08 12:04:17 -0300

    lógica de enviar a requisição Post iniciada

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -4,20 +4,42 @@ import { Link, useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+function Seat({name, isAvailable}){
+
+  return (
+    <>
+      
+    </>
+  )
+
+}
+
 export default function Seats() {
   const [seats, setSeats] = useState([])
   const [selected, setSelected] = useState([])
+  const [name, setUserName] = useState('')
+  const [cpf, setCpf] = useState('')
+
+  function toReserve(e) {
+    e.preventDefault();
+
+    const requisition = axios.post(
+      'https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many',
+      {
+        ids: [1, 2, 3],
+        name: 'Fulano',
+        cpf: '12345678900'
+      }
+    )
+  }
 
   function changeColor(seatClicked) {
-    console.log(seatClicked)
-    console.log(selected)
-
     if (!seatClicked.isAvailable) {
-      alert('Este assento não está disponível')
+      alert('Esse assento não está disponível')
       return
     }
 
-    if (selected.includes(seatClicked.id)){
+    if (selected.includes(seatClicked.id)) {
       const clicked = selected.filter(id => id !== seatClicked.id)
       setSelected(clicked)
       return
@@ -45,8 +67,12 @@ export default function Seats() {
           {Array.isArray(seats.seats) &&
             seats.seats.map(seat => (
               <div
-                //style={!seat.isAvailable ? { backgroundColor: 'red' } : null}
-                style={selected.includes(seat.id) ? { backgroundColor: 'green' } : null}
+                //style={!seat.isAvailable ? { backgroundColor: '#FBE192' } : null}
+                style={
+                  selected.includes(seat.id)
+                    ? { backgroundColor: '#1AAE9E' }
+                    : null
+                }
                 key={seat.id}
                 onClick={() => changeColor(seat)}
               >
@@ -68,23 +94,35 @@ export default function Seats() {
             <p>Indisponível</p>
           </div>
         </SeatsInfos>
-        <form>
+        <form onSubmit={toReserve}>
           <Inputs>
             <div>
               <p>Nome do comprador:</p>
-              <input type="text" placeholder="Digite seu nome..." />
+              <input
+                type="text"
+                value={name}
+                onChange={e => setUserName(e.target.value)}
+                placeholder="Digite seu nome..."
+                required
+              />
             </div>
             <div>
               <p>CPF do comprador:</p>
-              <input type="text" placeholder="Digite seu CPF..." />
+              <input
+                type="text"
+                value={cpf}
+                onChange={e => setCpf(e.target.value)}
+                placeholder="Digite seu CPF..."
+                required
+              />
             </div>
+            <Link to="/sucesso">
+              <Button type="submit" name="submit">
+                <p>Reservar assento(s)</p>
+              </Button>
+            </Link>
           </Inputs>
         </form>
-        <Link to="/sucesso">
-          <button type="submit" name="submit">
-            <p>Reservar assento(s)</p>
-          </button>
-        </Link>
       </SeatsContainer>
       {seats.movie ? (
         <Footer
@@ -103,23 +141,6 @@ const SeatsContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  button {
-    width: 225px;
-    height: 42px;
-    background: #e8833a;
-    border-radius: 3px;
-    border: none;
-    margin-top: 57px;
-  }
-
-  button > p {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    color: #ffffff;
-  }
 `
 const SeatContainer = styled.div`
   width: 350px;
@@ -131,7 +152,23 @@ const SeatContainer = styled.div`
   div {
     width: 26px;
     height: 26px;
-    background: #c3cfd9;
+    background: #c3cfd9; /* ${props => {
+      const available = props.availability
+      const clicked = props.selection
+      const seat = props.chair
+
+      if (!available) {
+        return '#FBE192'
+      } else {
+        return '#c3cfd9'
+      }
+
+      if (clicked.includes(seat)) {
+        return '#1AAE9E'
+      } else {
+        return '#c3cfd9'
+      }
+    }}; */
     border: 1px solid #808f9d;
     border-radius: 12px;
     display: flex;
@@ -211,5 +248,21 @@ const Inputs = styled.div`
     font-weight: 400;
     font-size: 18px;
     color: #afafaf;
+  }
+`
+const Button = styled.button`
+  width: 225px;
+  height: 42px;
+  background: #e8833a;
+  border-radius: 3px;
+  border: none;
+  margin-top: 57px;
+
+  p {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    color: #ffffff;
   }
 `
